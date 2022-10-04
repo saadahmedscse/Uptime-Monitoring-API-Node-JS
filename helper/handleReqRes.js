117690c5a5d4fed5b92c42268c592b02bd14a84f
Saad <saadahmedscse@gmail.com>
2022-10-04 17:54:34 +0600

    Handle request and response

---
+++ helper/handleReqRes.js
@@ -0,0 +1,41 @@
+/**
+ * Title: Handle Request Response
+ * Description: Handle Request and Response
+ * Author: Saad Ahmed
+ * Date: 04-Oct-2022
+ */
+
+//dependencies
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+
+//handle object - module scaffolding
+const handler = {}
+
+handler.handleReqRes = (req, res) => {
+    //request handle
+    //get url and parse
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+    const method = req.method.toLowerCase();
+    const queryObject = parsedUrl.query;
+    const headersObject = req.headers;
+  
+    const decoder = new StringDecoder("utf-8");
+    let realData = "";
+  
+    req.on("data", (buffer) => {
+      realData += decoder.write(buffer);
+    });
+  
+    req.on("end", () => {
+      realData += decoder.end();
+      console.log(realData);
+    });
+  
+    //response handle
+    res.end("Hello World");
+  };
+
+module.exports = handler;
