79536c0a15f69d5d65659e590643f57b349ac422
Saad <saadahmedscse@gmail.com>
2022-10-04 18:26:55 +0600

    added not found handler

---
+++ handlers/routeHandlers/notFoundHandler.js
@@ -0,0 +1,15 @@
+/**
+ * Title: Not Found Handler
+ * Description: 404 Not Found Handler
+ * Author: Saad Ahmed
+ * Date: 04-Oct-2022
+ */
+
+//module scaffolding
+const handler = {};
+
+handler.notFoundHandler = () => {
+  console.log("Not found");
+};
+
+module.exports = handler;
