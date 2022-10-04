e1268c7cab859ff4fb21a4c1af24c6cd6482f9bc
Saad <saadahmedscse@gmail.com>
2022-10-04 18:27:04 +0600

    added sample route handler

---
+++ handlers/routeHandlers/sampleRouteHandler.js
@@ -0,0 +1,15 @@
+/**
+ * Title: Sample Handler
+ * Description: Sample Handler
+ * Author: Saad Ahmed
+ * Date: 04-Oct-2022
+ */
+
+//module scaffolding
+const handler = {}
+
+handler.sampleHandler = () => {
+    console.log("sample")
+}
+
+module.exports = handler;
