cd04764a9b0f89271858056da74013c0f7e93499
Saad <saadahmedscse@gmail.com>
2022-10-04 18:26:48 +0600

    Added routes

---
+++ routes.js
@@ -0,0 +1,16 @@
+/**
+ * Title: API Routes
+ * Description: API Routes has been declared here
+ * Author: Saad Ahmed
+ * Date: 04-Oct-2022
+ */
+
+const {
+  sampleHandler,
+} = require("./handlers/routeHandlers/sampleRouteHandler");
+
+//dependencies
+
+const route = {
+  sample: sampleHandler,
+};
