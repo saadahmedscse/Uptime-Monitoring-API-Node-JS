8dedeaea3e4fd73a5978c62d33d2052ec2b1037c
Saad <saadahmedscse@gmail.com>
2022-10-10 15:58:07 +0600

    read data using query object

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -7,6 +7,7 @@
 
 //dependencies
 const data = require("../../lib/data");
+const utils = require("../../helper/utils");
 
 //module scaffolding
 handler = {};
@@ -30,31 +31,29 @@ handler._user.post = (requestProperties, callback) => {
 
   const firstName =
     typeof requestProperties.body.firstName === "string" &&
-    requestProperties.body.firstName.trim().lenght > 0
+    requestProperties.body.firstName.trim().length > 0
       ? requestProperties.body.firstName
       : false;
 
   const lastName =
-    typeof body.lastName === "string" && body.lastName.trim().lenght > 0
+    typeof body.lastName === "string" && body.lastName.trim().length > 0
       ? body.lastName
       : false;
 
   const phone =
-    typeof body.phone === "string" && body.phone.trim().lenght === 11
+    typeof body.phone === "string" && body.phone.trim().length === 11
       ? body.phone
       : false;
 
   const password =
-    typeof body.password === "string" && body.password.trim().lenght > 0
+    typeof body.password === "string" && body.password.trim().length > 0
       ? body.password
       : false;
 
   const tosAgreement =
-    typeof body.tosAgreement === "boolean" && body.password.trim().lenght > 0
+    typeof body.tosAgreement === "boolean" && body.password.trim().length > 0
       ? body.tosAgreement
       : false;
-
-  console.log(!firstName);
 
   if (!firstName) {
     callback(400, {
@@ -76,13 +75,8 @@ handler._user.post = (requestProperties, callback) => {
       status: "Failed",
       message: "Password is required",
     });
-  } else if (!tosAgreement) {
-    callback(400, {
-      status: "Failed",
-      message: "Invalid terms of service agreement",
-    });
   } else {
-    data.read(phone, (err, user) => {
+    data.read(phone, (err, _) => {
       if (err) {
         const fullName = `${firstName} ${lastName}`;
         const userObj = {
@@ -90,7 +84,7 @@ handler._user.post = (requestProperties, callback) => {
           lastName,
           fullName,
           phone,
-          password,
+          password: utils.getHash(password),
           tosAgreement,
         };
         data.create(phone, userObj, (err) => {
@@ -117,10 +111,35 @@ handler._user.post = (requestProperties, callback) => {
 };
 
 handler._user.get = (requestProperties, callback) => {
-  callback(200, {
-    status: "Success",
-    message: "This is a get method",
-  });
+  const queryObj = requestProperties.queryObject;
+
+  const phone =
+    typeof queryObj.phone === "string" && queryObj.phone.length === 11
+      ? queryObj.phone
+      : false;
+
+  if (phone) {
+    data.read(phone, (err, userData) => {
+      if (!err) {
+        const user = utils.parseJson(userData);
+        delete user.password;
+        callback(200, {
+          status: "Success",
+          data: user,
+        });
+      } else {
+        callback(400, {
+          status: "Failed",
+          message: "No user found with this number",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      status: "Failed",
+      message: "Invalid Phone number",
+    });
+  }
 };
 
 handler._user.put = (requestProperties, callback) => {
