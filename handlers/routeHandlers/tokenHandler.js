8af8484f04d270d810c844474af79ff7d8d983b6
Saad <saadahmedscse@gmail.com>
2022-10-11 11:16:54 +0600

    create new token

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -15,7 +15,7 @@ handler = {};
 handler.tokenHandler = (requestProperties, callback) => {
   const acceptedMethods = ["post", "get", "put", "delete"];
   if (acceptedMethods.indexOf(requestProperties.method) > -1) {
-    handler._user[requestProperties.method](requestProperties, callback);
+    handler._token[requestProperties.method](requestProperties, callback);
   } else {
     callback(405, {
       status: "Failed",
@@ -26,7 +26,71 @@ handler.tokenHandler = (requestProperties, callback) => {
 
 handler._token = {};
 
-handler._token.post = (requestProperties, callback) => {};
+handler._token.post = (requestProperties, callback) => {
+  const body = requestProperties.body;
+
+  const phone =
+    typeof body.phone === "string" && body.phone.trim().length === 11
+      ? body.phone
+      : false;
+
+  const password =
+    typeof body.password === "string" && body.password.trim().length > 0
+      ? body.password
+      : false;
+
+  if (!phone) {
+    callback(400, {
+      status: "Failed",
+      message: "Invalid Phone number length",
+    });
+  } else if (!password) {
+    callback(400, {
+      status: "Failed",
+      message: "Password is required",
+    });
+  } else {
+    data.read(phone, (err, userData) => {
+      if (!err) {
+        let hashedPass = utils.getHash(password);
+        let userPass = utils.parseJson(userData).password;
+        if (hashedPass === userPass) {
+          let token = utils.getRandomToken(16);
+          const expires = Date.now() * 60 * 60 * 1000;
+          const tokenObj = {
+            phone,
+            token,
+            expires,
+          };
+
+          data.create("token " + phone, tokenObj, (err) => {
+            if (!err) {
+              callback(200, {
+                status: "Success",
+                message: "Token has been created successfully",
+              });
+            } else {
+              callback(500, {
+                status: "Failed",
+                message: err,
+              });
+            }
+          });
+        } else {
+          callback(400, {
+            status: "Failed",
+            message: "The password you entered is invalid",
+          });
+        }
+      } else {
+        callback(400, {
+          status: "Failed",
+          message: "No user found with this phone number",
+        });
+      }
+    });
+  }
+};
 
 handler._token.get = (requestProperties, callback) => {};
 
